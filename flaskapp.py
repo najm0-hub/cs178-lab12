1f8b197d5197c09ffd73f2d36dc654ce80679f38
Najmo <najmo.mahamed@drake.edu>
2026-04-18 12:51:14 -0500

    exercise one

--- flaskapp.py
+++ flaskapp.py
@@ -28,6 +28,14 @@ def hello(name):
 #  YOUR ROUTES GO BELOW THIS LINE
 #  Each exercise asks you to add a new @app.route here
 # ============================================================
+# ---- Exercise 1: add this to flaskapp.py ----
+
+@app.route('/analyze/<word>')
+def analyze(word):
+    # YOUR CODE HERE
+    # Count the characters in `word` and return as a string
+    # Example: /analyze/Drake  →  should display: 5
+    pass
 
 
 
